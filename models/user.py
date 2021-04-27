7d2a44a584b0702bcdfecfc48f6d2858628f72c8
samj0516 <samj0516@gmail.com>
2021-04-27 11:12:24 -0500

    changed user.py in models

--- models/user.py
+++ models/user.py
@@ -3,15 +3,15 @@ from datetime import date
 class User():
     def __init__(self, id, first_name, last_name, email, password):
         self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
+        self.firstName = first_name
+        self.lastName = last_name
         self.email = email
         self.password = password
         self.bio = "No bio yet"
-        self.profile_image_url = ""
-        self.created_on = date.today()
+        self.profileImageUrl = ""
+        self.createdOn = date.today()
         self.active = 1
-        self.is_staff = 1
+        self.isStaff = 1
 
 
 @property
